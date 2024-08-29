015954c794f56d368c7ab6d7060e176a418ed788
Songpon <tum@LM000C6PVWHYYCT.local>
2024-08-29 22:04:18 +0700

    first commit

---
+++ src/main/java/io/songpon/ping_pong/PingController.java
@@ -0,0 +1,13 @@
+package io.songpon.ping_pong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
---
+++ src/test/java/io/songpon/ping_pong/PingPongApplicationTests.java
@@ -0,0 +1,13 @@
+package io.songpon.ping_pong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
